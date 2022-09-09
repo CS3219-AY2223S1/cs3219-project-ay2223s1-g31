8f4752ae2e663d9a1fe3c979c8dd516c4f8fd373
pbthang <bathang02@gmail.com>
2022-09-10 01:21:24 +0800

    Handle duplicate user when signing up

--- user-service/model/user-orm.js
+++ user-service/model/user-orm.js
@@ -1,14 +1,17 @@
-import { createUser } from './repository.js';
+import { createUser, userExists } from "./repository.js";
 
 //need to separate orm functions from repository to decouple business logic from persistence
 export async function ormCreateUser(username, password) {
-    try {
-        const newUser = await createUser({username, password});
-        newUser.save();
-        return true;
-    } catch (err) {
-        console.log('ERROR: Could not create new user');
-        return { err };
+  try {
+    const isUserExisted = userExists({ username });
+    if (isUserExisted) {
+      return false;
     }
+    const newUser = await createUser({ username, password });
+    newUser.save();
+    return true;
+  } catch (err) {
+    console.log("ERROR: Could not create new user");
+    return { err };
+  }
 }
-
