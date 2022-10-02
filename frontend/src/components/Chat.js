491046a530ec1300431130b3c5ea4b4f0ecb773f
Radhya <mradhyaf@gmail.com>
2022-10-02 14:57:22 +0800

    Change user in chat component to authContext username

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,13 +1,14 @@
 import { Box, Button, List, ListItem, ListItemText, TextField } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { io } from "socket.io-client";
+import { useAuth } from '../utils/AuthContext';
 
 
-const user = 'Jimmy Neutron'
 const COMM_SVC_URL = 'http://localhost:8002'
 const socket = io(COMM_SVC_URL)
 
 function Chat() {
+  const { auth } = useAuth()
   const [messages, setMessages] = useState([
     {
       user: 'admin',
@@ -19,7 +20,7 @@ function Chat() {
   const [roomId, setRoomId] = useState(0)
 
   const sendMessage = () => {
-    let msg = {user: user, text: inputMessage}
+    let msg = {user: auth.username, text: inputMessage}
     setMessages((old) => [...old, msg])
     
     socket.emit('send-message', msg, roomId)
