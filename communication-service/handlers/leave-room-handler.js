b3c0757610baa158f7d361dd14f15f3aaeeb0663
Radhya <mradhyaf@gmail.com>
2022-10-16 16:48:46 +0800

    Fix import bug in handler

--- communication-service/handlers/leave-room-handler.js
+++ communication-service/handlers/leave-room-handler.js
@@ -1,4 +1,4 @@
-import { ormDelRoom } from "../models/room-orm";
+import { ormDelRoom } from "../models/room-orm.js";
 
 export function leaveRoomHandler(io, socket) {
   return async ({ roomId }) => {
